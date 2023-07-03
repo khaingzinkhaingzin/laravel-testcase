5ea0f89ffc6e658cbab8e4ea6c9547241bd0186d
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2023-07-03 23:01:23 +0630

    Laravel with feature testing

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/');
+        
+        $response->assertStatus(200);
+    }
+
+    public function test_the_homepage_contains_symfony(): void
+    {
+        $response = $this->get('/');
+        
+        $response->assertSee('Symfony');
+
+        $response->assertStatus(200);
+    }
+}
